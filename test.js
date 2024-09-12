8c751106c705208dd69c0584c0ec64f1086dbc48
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-09-12 13:33:24 +0200

    Expose `decoder.remaining` (#3)

--- test.js
+++ test.js
@@ -5,13 +5,17 @@ test('utf8, 1-byte characters', (t) => {
   const td = new TextDecoder()
 
   t.is(td.push(Buffer.from('hello world')), 'hello world')
+  t.is(td.remaining, 0)
 })
 
 test('utf8, 2-byte character', (t) => {
   const td = new TextDecoder()
 
   t.is(td.push(Buffer.of(0xc2)), '')
+  t.is(td.remaining, 1)
+
   t.is(td.push(Buffer.of(0xa2)), '¢')
+  t.is(td.remaining, 0)
 
   t.is(td.push('hello world'), 'hello world', 'remainder')
 })
@@ -20,8 +24,13 @@ test('utf8, 3-byte character', (t) => {
   const td = new TextDecoder()
 
   t.is(td.push(Buffer.of(0xe2)), '')
+  t.is(td.remaining, 1)
+
   t.is(td.push(Buffer.of(0x82)), '')
+  t.is(td.remaining, 2)
+
   t.is(td.push(Buffer.of(0xac)), '€')
+  t.is(td.remaining, 0)
 
   t.is(td.push('hello world'), 'hello world', 'remainder')
 })
@@ -30,9 +39,16 @@ test('utf8, 4-byte character', (t) => {
   const td = new TextDecoder()
 
   t.is(td.push(Buffer.of(0xf0)), '')
+  t.is(td.remaining, 1)
+
   t.is(td.push(Buffer.of(0x9f)), '')
+  t.is(td.remaining, 2)
+
   t.is(td.push(Buffer.of(0x92)), '')
+  t.is(td.remaining, 3)
+
   t.is(td.push(Buffer.of(0xa9)), '💩')
+  t.is(td.remaining, 0)
 
   t.is(td.push('hello world'), 'hello world', 'remainder')
 })
